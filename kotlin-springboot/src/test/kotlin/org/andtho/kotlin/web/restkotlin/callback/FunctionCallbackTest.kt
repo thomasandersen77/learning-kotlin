a3646d8e2d596881ff2cfdc4bba604fbddbf6c39
Thomas Andersen <thomas.andersen@knowit.no>
2018-10-01 23:49:39 +0200

    more high order functions

--- kotlin-springboot/src/test/kotlin/org/andtho/kotlin/web/restkotlin/callback/FunctionCallbackTest.kt
+++ kotlin-springboot/src/test/kotlin/org/andtho/kotlin/web/restkotlin/callback/FunctionCallbackTest.kt
@@ -1,25 +1,52 @@
 package org.andtho.kotlin.web.restkotlin.callback
 
 import org.junit.Test
+import java.io.File
+import javax.swing.JDialog
 import javax.swing.JOptionPane
+import kotlin.concurrent.thread
 
 class FunctionCallbackTest {
+
+    fun showMessageDialog(text: String) : () -> JDialog = {
+        val pane = JOptionPane()
+        pane.messageType = JOptionPane.PLAIN_MESSAGE
+        pane.message = text
+        val dialog = pane.createDialog(text)
+        thread(start = true){
+            Thread.sleep(1000)
+            dialog.dispose()
+        }
+        dialog.isVisible = true
+        dialog
+    }
+
     @Test
     fun `use handler to show message dialog`() {
-        val handler = Handler()
-        fun showMessageDialog(text : String) : () -> Unit = {
-            JOptionPane.showMessageDialog(null, text)
-        }
-
-        val result = handler.execute(showMessageDialog("Hello World!!"), object : FunctionCallback() {
+        Handler.execute(showMessageDialog("Hello World!!"), object : FunctionCallback() {
             override fun onSuccess() {
-                println("onSuccess Callbakc")
+                println("execute onSuccess")
+            }
+            override fun onFailure() {
+                println("execute onFailure")
+            }
+        })
+    }
+
+    fun writeFile(content : String, filename : String) : () -> Unit = {
+        File(filename).writeText(content)
+    }
+
+    @Test
+    fun `write file via highorder function`() {
+        Handler.execute(writeFile("Kotlin rules", "target/textfile.txt"), object : FunctionCallback() {
+            override fun onSuccess() {
+                println("execute onSuccess")
             }
             override fun onFailure() {
                 println("execute onFailure")
             }
         })
 
-        println(result)
     }
 }
